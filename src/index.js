6e97a5f07608801797fe1b171f29b67246b621c9
johnwalley <john@walley.org.uk>
2019-01-14 21:24:40 +0000

    Demo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ function sum(array) {
   let result;
 
   for (const value of array) {
-    if (value !== undefined) { result = result === undefined ? value : result + value; }
+    if (value !== undefined) {
+      result = result === undefined ? value : result + value;
+    }
   }
-
-  return result
+  return result;
 }
 
 sum([1, 2, 3, 4, 5]);
